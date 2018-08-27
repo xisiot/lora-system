f074e7ba1ec3252eb7bb42e041e0a98a414471af
“jinyuxing” <2013211951@bupt.edu.cn>
2018-08-27 11:31:47 +0800

    add lora-network-controller

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 cp config.json lora-network-server/config/config.json
 cp config.json lora-network-connector/config/config.json
 cp config.json lora-join-server/config/config.json
+cp config.json lora-network-controller/config/config.json
 
 pm2 start pm2.json
