21415a05eb93e7f66024d083968f22ba74587ed9
“jinyuxing” <2013211951@bupt.edu.cn>
2018-08-27 17:14:29 +0800

    rm application server

--- start.sh
+++ start.sh
@@ -4,6 +4,5 @@ cp config.json lora-network-server/config/config.json
 cp config.json lora-network-connector/config/config.json
 cp config.json lora-join-server/config/config.json
 cp config.json lora-network-controller/config/config.json
-cp config.json lora-application-server/config/config.json
 
 pm2 start pm2.json
